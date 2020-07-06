f10ba2f982f3845f787c3a2bdc19bae90eca916f
DanielEspinosa-hub <67650788+DanielEspinosa-hub@users.noreply.github.com>
2020-07-06 14:56:59 -0500

    Add files via upload

---
+++ Script_de_ejemplo_Proyecto_Coursera.R
@@ -0,0 +1,4 @@
+# Daniel Espinosa Martínez
+# Script de ejemplo Proyecto Coursera
+# Ejemplo de graficas en RStudio. Histograma
+hist(Datos$WEIGHT,main="Distribucion de peso de los osos",col="#996600",border="#ff9900",breaks=10,xlab="Peso (kg)",ylab="Nº de osos")
